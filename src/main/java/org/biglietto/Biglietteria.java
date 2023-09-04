a44940519e60ac993c3c8e776e57d99e5fa00f95
francesca <francipeco36@gmail.com>
2023-09-04 13:05:24 +0200

    milestone 3 completed

--- src/main/java/org/biglietto/Biglietteria.java
+++ src/main/java/org/biglietto/Biglietteria.java
@@ -1,6 +1,7 @@
 package org.biglietto;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Biglietteria {
@@ -13,11 +14,29 @@ public class Biglietteria {
             int km = Integer.parseInt(scan.nextLine());
             System.out.println("Età: ");
             int eta= Integer.parseInt(scan.nextLine());
+            System.out.println("Data del viaggio: ");
+            LocalDate data = LocalDate.parse(scan.nextLine());
+
+            System.out.println("Tipologia biglietto (normale/flessibile): ");
+            String tipo = scan.nextLine();
+            boolean flessibile=false;
+            switch (tipo) {
+                case "normale":
+                    flessibile=false;
+                    break;
+                case "flessibile":
+                    flessibile=true;
+                    break;
+                default:
+                    System.out.println("Scelta non valida.");
+            }
+
             try {
-                Biglietto biglietto = new Biglietto(km, eta);
+                Biglietto biglietto = new Biglietto(km, eta, data, flessibile);
                 isValid = true;
-                System.out.println("Biglietto valido");
-                System.out.println("Il prezzo del tuo biglietto è: " + biglietto.calcolaPrezzo(km, eta) + " €.");
+                System.out.println("Ecco il tuo biglietto:");
+                System.out.println("Prezzo: " + biglietto.calcolaPrezzo(km, eta) + " €.");
+                System.out.println("Scadenza biglietto: " + biglietto.calcolaDataScadenza(data, flessibile));
             } catch (RuntimeException e) {
                 System.out.println("Il numero di km e l'età devono essere maggiori di 0.");
             }
--- src/main/java/org/biglietto/Biglietto.java
+++ src/main/java/org/biglietto/Biglietto.java
@@ -1,8 +1,22 @@
 package org.biglietto;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Biglietto {
+
+    /* Alla classe Biglietto aggiungere i seguenti attributi:
+● data: LocalDate
+● flessibile: boolean
+Entrambi gli attributi vanno valorizzati nel costruttore.
+Aggiungere due costanti:
+● durata normale: 30 giorni (int)
+● durata flessibile: 90 giorni (int)
+ Nel metodo che calcola il prezzo, se il biglietto è flessibile, maggiorare il prezzo del 10%.
+Modificare la classe Biglietteria in modo che, alla creazione del Biglietto, valorizzi la data con la data odierna e il parametro flessibile in base alla scelta dell’utente. Dopo aver stampato il prezzo del biglietto, stampare a video anche la data di scadenza.
+*/
+
+    // ATTRIBUTI
  private int km;
  private int eta;
 
@@ -10,17 +24,22 @@ public class Biglietto {
  private final BigDecimal SCONTO_OVER_65 = new BigDecimal("0.6");
  private final BigDecimal SCONTO_MINORI = new BigDecimal("0.8");
 
+ private LocalDate data;
+ private boolean flessibile;
+ private final int DURATA_BIGLIETTO_NORMALE = 30;
+ private final int DURATA_BIGLIETTO_FLESSIBILE = 90;
 
     //COSTRUTTORI
-public Biglietto(int km, int eta) throws RuntimeException {
+public Biglietto(int km, int eta, LocalDate data, boolean flessibile) throws RuntimeException {
   this.km=km;
   this.eta= eta;
   isValidKm(this.km);
   isValidEta(this.eta);
+  this.data= data;
+  this.flessibile=flessibile;
 }
 
     // METODI
-    // isValidKm: se non valido sollevare eccezione
 private boolean isValidKm(int km) {
         if (km<=0){
             throw new RuntimeException();
@@ -28,7 +47,6 @@ private boolean isValidKm(int km) {
         return true;
     }
 
-    // isValidEta: se non valido sollevare eccezione
     private boolean isValidEta(int eta) {
         if (eta<0){
             throw new RuntimeException();
@@ -36,7 +54,6 @@ private boolean isValidKm(int km) {
         return true;
     }
 
-    //calcolaSconto: metodo private
 private BigDecimal calcolaSconto(int eta){
     BigDecimal sconto = new BigDecimal("1");
     if(eta<18){
@@ -47,10 +64,20 @@ private BigDecimal calcolaSconto(int eta){
     return sconto;
 }
 
-    // calcolaPrezzo: calcola il prezzo del biglietto comprensivo di eventuale sconto; richiama metodo calcolaSconto
     public BigDecimal calcolaPrezzo(int km, int eta){
         BigDecimal kmViaggio = BigDecimal.valueOf(km);
         BigDecimal prezzo =kmViaggio.multiply(COSTO_KM).multiply(calcolaSconto(eta));
         return prezzo;
+    }
+    /*Aggiungere un metodo (calcolaDataScadenza: LocalDate) che calcola la data di scadenza del biglietto, applicando la durata normale o flessibile in base al parametro flessibile(boolean).*/
+
+    public LocalDate calcolaDataScadenza(LocalDate data, boolean flessibile){
+        LocalDate dataScadenza;
+        if (flessibile=false){
+           dataScadenza=data.plusDays(DURATA_BIGLIETTO_NORMALE);
+        } else {
+            dataScadenza=data.plusDays(DURATA_BIGLIETTO_FLESSIBILE);
+        }
+            return dataScadenza;
     }
 }
