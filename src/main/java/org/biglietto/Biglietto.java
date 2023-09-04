fc31410f44be7a0b410c0d16566b2b0fd5971983
francesca <francipeco36@gmail.com>
2023-09-04 09:42:51 +0200

    milestone 1: file preparation

---
+++ src/main/java/org/biglietto/Biglietto.java
@@ -0,0 +1,26 @@
+package org.biglietto;
+
+public class Biglietto {
+    // ATTRIBUTI: km e età passeggero
+    /*Aggiungere tre costanti:
+    il costo chilometrico di 21 centesimi di €/km (BigDecimal),
+    lo sconto over 65 del 40 % (BigDecimal)
+    lo sconto minorenni del 20% (BigDecimal).*/
+
+
+    //COSTRUTTORI
+
+
+    // METODI
+    // isValidKm: se non valido sollevare eccezione
+
+    // isValidEta: se non valido sollevare eccezione
+
+
+    //calcolaSconto: metodo private
+
+
+    // calcolaPrezzo: calcola il prezzo del biglietto comprensivo di eventuale sconto; richiama metodo calcolaSconto
+
+
+ }
